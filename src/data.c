82be8f7214b86e6e18037b1245bf9c3f11052dd8
PedroTFX <pedro.trindade.tfx@gmail.com>
2022-09-22 09:03:19 +0100

    tests done, uncertainty only on data destroy

--- src/data.c
+++ src/data.c
@@ -1,6 +1,7 @@
 #include <data.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Função que cria um novo elemento de dados data_t, reservando a memória
  * necessária para armazenar os dados, especificada pelo parâmetro size
@@ -31,29 +32,10 @@ struct data_t *data_create(int size){
  * memória para os dados.
  */
 struct data_t *data_create2(int size, void *data){
-		//unable to reserve that amount of memory
-	if(size <= 0){ 
-		return NULL;
-	}
-	
-	struct data_t *data_st = (struct data_t*)malloc(sizeof(struct data_t));
-
-	//error on init
-	if(data == NULL){
-		return NULL;
-	}
-
-	data_st->data = malloc(size);
-	data_st->datasize = size;
-	
-	//error on memory reserve
-	if(data_st->data == NULL){
-		free(data_st);
-		return NULL;
-	}
+	struct data_t *data_st = data_create(size);
 
 	//copy mem to data
-	memcpy(data, data_st, size);
+	memcpy(data_st->data, data, size);
 	return data_st;
 }
 
@@ -76,11 +58,8 @@ struct data_t *data_dup(struct data_t *data){				//for later review
  *  Deve assegurar que destroi o conteúdo antigo do mesmo.
  */
 void data_replace(struct data_t *data, int new_size, void *new_data){
-	//clean prev data
-	volatile char *ucptr = data;
-    if(ucptr) while(data->datasize--) *ucptr++ = 0;
-    asm("");
-    free(data);
+	//clean prev data->data
+    free(data->data);
 
 	//put new data
 	data->datasize = new_size;
@@ -88,20 +67,39 @@ void data_replace(struct data_t *data, int new_size, void *new_data){
 
 	if(data->data == NULL){
 		free(data);
-		return NULL;
+		return; // a func é um void?? n se rpepara erros?
 	}
 
 	memcpy(data, new_data, new_size);
 }
 
-void print(){
-	printf("%d", 3);
-}
-
 int main(int argc, char const *argv[])
 {
+	//test data_create
 	struct data_t *data = data_create(0);
-	printf("%p", data->data);
+	printf("datasize: %d\n", data->datasize);
+	printf("data(pointer): %p\n", data->data);
+	
+	//test data_create2
+	struct data_t *data1 = data_create2(4, data);
+	printf("datasize: %d\n", data1->datasize);
+	printf("data(pointer): %p\n", data1->data);
+	
+	//test data_replace
+	data_replace(data, data1->datasize, data1);
+	printf("datasize: %d\n", data->datasize);
+	printf("data(pointer): %p\n", data->data);
+	
+	//test data_duplicate
+	struct data_t *data2 = data_dup(data1);
+	printf("datasize: %d\n", data2->datasize);
+	printf("data(pointer): %p\n", data2->data);
+	
+	//test data_destroy
+	data_destroy(data2);
+	printf("datasize: %d\n", data2->datasize);
+	printf("data(pointer): %p\n", data2->data);
+	
 	return 0;
 }
 
