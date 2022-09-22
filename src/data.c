34301049e7bd085b19055de00f624a9e957b389f
PedroTFX <pedro.trindade.tfx@gmail.com>
2022-09-22 08:06:16 +0100

    major progress on data.c (yet to be revisioned and tested)

--- src/data.c
+++ src/data.c
@@ -69,18 +69,29 @@ void data_destroy(struct data_t *data){
  * necessária para a nova estrutura, inclusivamente dados.
  */
 struct data_t *data_dup(struct data_t *data){				//for later review
-	/*struct data_t *data2 = (struct data_t *)malloc(data->datasize);
-	data2->data = data->data;
-	return data2; */
+	return data_create2(data->datasize, data->data);
 }
 
 /* Função que substitui o conteúdo de um elemento de dados data_t.5
  *  Deve assegurar que destroi o conteúdo antigo do mesmo.
  */
 void data_replace(struct data_t *data, int new_size, void *new_data){
-	/*free(data->data);
-	data = (struct data_t *)malloc(new_size);
-	data->data = new_data;*/
+	//clean prev data
+	volatile char *ucptr = data;
+    if(ucptr) while(data->datasize--) *ucptr++ = 0;
+    asm("");
+    free(data);
+
+	//put new data
+	data->datasize = new_size;
+	data->data = malloc(new_size);
+
+	if(data->data == NULL){
+		free(data);
+		return NULL;
+	}
+
+	memcpy(data, new_data, new_size);
 }
 
 void print(){
