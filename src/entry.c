1489d929b8a990030c9087638821b289ff79ae47
PedroTFX <pedro.trindade.tfx@gmail.com>
2022-09-25 19:00:46 +0100

    fixed free(entry->key)

--- src/entry.c
+++ src/entry.c
@@ -26,6 +26,7 @@ struct entry_t *entry_create(char *key, struct data_t *data) {
 */
 void entry_replace(struct entry_t *entry, char *new_key, struct data_t *new_value){
     data_destroy(entry->value);
+	free(entry->key);
     entry->key = new_key;
     entry->value = new_value;
 }
